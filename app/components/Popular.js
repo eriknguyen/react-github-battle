9c20042e7ee28a47a563d695a1c7acaf2f2492c5
Erik Nguyen <namkhanh164@gmail.com>
2017-06-06 23:12:00 +0800

    Life cycle events and methods

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage (props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -18,6 +19,92 @@ function SelectLanguage (props) {
     </ul>
   )
 }
+
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
+
+
+function RepoGrid (props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function (repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'> #{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img 
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                  />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
+
+class Popular extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All',
+      repos: null
+    };
+
+    // we want the updateLanguage() method to always be invoked on the specific context
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  updateLanguage (lang) {
+    this.setState(function() {
+      return {
+        selectedLanguage: lang,
+        repos: null
+      }
+    })
+    api.fetchPopularRepos(lang)
+      .then(function (repos) {
+        this.setState(function() {
+          return {
+            repos: repos
+          }
+        })      
+      }.bind(this));
+  }
+
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage} />
+        {!this.state.repos 
+        ? <p>Loading...</p>
+        : <RepoGrid repos={this.state.repos} /> }
+      </div>
+    )
+  }
+}
+
 
 /* class component */
 // class SelectLanguage extends React.Component {
@@ -39,40 +126,5 @@ function SelectLanguage (props) {
 //     )
 //   }
 // }
-
-SelectLanguage.propTypes = {
-  selectedLanguage: PropTypes.string.isRequired,
-  onSelect: PropTypes.func.isRequired
-}
-
-class Popular extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      selectedLanguage: 'All'
-    };
-
-    // we want the updateLanguage() method to always be invoked on the specific context
-    this.updateLanguage = this.updateLanguage.bind(this);
-  }
-
-  updateLanguage (lang) {
-    this.setState(function() {
-      return {
-        selectedLanguage: lang
-      }
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <SelectLanguage
-          selectedLanguage={this.state.selectedLanguage}
-          onSelect={this.updateLanguage} />
-      </div>
-    )
-  }
-}
 
 module.exports = Popular;
