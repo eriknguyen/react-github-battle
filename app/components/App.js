d713d717ff5039297fa192105434ddf0bc3afc7e
Erik Nguyen <namkhanh164@gmail.com>
2017-06-07 00:26:26 +0800

    add navigation bar with React-Router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,29 @@
 var React = require('react');
 var Popular = require('./Popular');
+var Battle = require('./Battle');
+var Nav = require('./Nav');
+var Home = require('./Home');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className="container">
-				<Popular />
-			</div>
+			<Router>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/popular' component={Popular} />
+            <Route exact path='/battle' component={Battle} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
 		)
 	}
 }
