7faaab666d7682fd7b8714116b04b4cbc56782e6
Erik Nguyen <namkhanh164@gmail.com>
2017-06-08 19:11:43 +0800

    prepare api call to get userdata: axios, promises, github api, .reduce(), .sort()

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,7 +2,6 @@ var React = require('react');
 
 class Results extends React.Component {
   render() {
-    console.log(this.props);
     return (
       <div>
         Battle
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,79 @@
 var axios = require('axios');
 
+var id = 'YOUR_GITHUB_CLIENT_ID';
+var secret = 'YOUR_SECRET_ID';
+var params = '?client_id' + id + '&client_secret=' + secret;
+
+
+function getProfile (username) {
+  return axios.get('https://api.github.com/com/users/' + username + params)
+    .then(function (user) {
+      return user.data;
+    });
+}
+
+function getRepo (username) {
+  return axios.get('https://api.github.com/com/users/' + username + '/repos' + params + '&per_page=100')
+}
+
+function getStarCount (repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore (profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return (followers*3) + totalStars;
+}
+
+function handleError (error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData (player) {
+  // .all() takes in array of promises, once all promises resolved => then execute function
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(function (data) {
+    var profile = data[0];  //data[0] returned from first promises
+    var repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+}
+
+function sortPlayers (players) {
+  return players.sort(function (a, b) {
+    return b.score - a.score;
+  })
+}
+
+/* 
+Example
+
+api.battle(['tyler', 'erik'])
+  .then(function (players) {
+    console.log(players[0]);
+    console.log(players[1]);
+  })
+
+*/
+
+
 module.exports = {
+  battle: function (players) {
+    return axios.all(players.map(getUserData))  //return array of promises, each is getUserData(player), return array of userData
+      .then(sortPlayers)  //sort over userData
+      .catch(handleError);
+  },
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
 
