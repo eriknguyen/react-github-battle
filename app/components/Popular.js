e53efb70244d5ce478075bf80a6538f6dee727b9
Erik Nguyen <namkhanh164@gmail.com>
2017-06-10 00:13:42 +0800

    built reusable loading component, deployed to firebase hosting

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 function SelectLanguage (props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -98,7 +99,7 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage} />
         {!this.state.repos 
-        ? <p>Loading...</p>
+        ? <Loading text='Downloading' speed={100}/>
         : <RepoGrid repos={this.state.repos} /> }
       </div>
     )
